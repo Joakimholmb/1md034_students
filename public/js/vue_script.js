7a54f9080890d652bca8ac71f1bc23bad0a3e298
Joakim <joakimeskils@gmail.com>
2019-02-08 08:08:56 +0100

    nu funkar kartan på bägge med addorder

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,7 @@
 /*jslint es5:true, indent: 2 */
 /*global Vue, io */
 /* exported vm */
+
 var socket = io();
 
 var vueObject = new Vue({
@@ -16,31 +17,35 @@ var vueObject = new Vue({
         orders: {},
         form: null
     },
+    created: function () {
+      socket.on('initialize', function (data) {
+        this.orders = data.orders;
+      }.bind(this));
+  
+      socket.on('currentQueue', function (data) {
+        this.orders = data.orders;
+      }.bind(this));
+    },
     methods: {
         update: function(){
             console.log("Button clicked!");
             this.form = {checked: this.checked, fullname: this.fullname, email: this.email, payment: this.payment, gender: this.gender, coordinates: this.coordinates};
-            this.addOrder();
+            //this.addOrder;
         },
-        created: function () {
-            socket.on('initialize', function (data) {
-              this.orders = data.orders;
-            }.bind(this));
-        
-            socket.on('currentQueue', function (data) {
-              this.orders = data.orders;
-            }.bind(this));
-          },
         getNext: function () {
             var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
               return Math.max(last, next);
             }, 0);
             return lastOrder + 1;
           },
-          addOrder: function() {
-            socket.emit("addOrder", {orderId: '1',
-                                    details: { x: this.coordinates.x, y: this.coordinates.y}}
-                                    );
+          addOrder: function (event) {
+            var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                          y: event.currentTarget.getBoundingClientRect().top};
+            socket.emit("addOrder", { orderId: this.getNext(),
+                                      details: { x: event.clientX - 10 - offset.x,
+                                                 y: event.clientY - 10 - offset.y },
+                                      orderItems: ["Beans", "Curry"]
+                                    });
           },
           displayOrder: function (event) {
             var offset = {x: event.currentTarget.getBoundingClientRect().left,
