1187fbf00ec71a1df0d1d67f2400eea927aad065
Joakim <joakimeskils@gmail.com>
2019-02-08 08:16:56 +0100

    before last messaging task

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -16,7 +16,7 @@ var vueObject = new Vue({
         coordinates: {},
         orders: {},
         form: null
-    },
+    },/*
     created: function () {
       socket.on('initialize', function (data) {
         this.orders = data.orders;
@@ -25,12 +25,12 @@ var vueObject = new Vue({
       socket.on('currentQueue', function (data) {
         this.orders = data.orders;
       }.bind(this));
-    },
+    },*/
     methods: {
         update: function(){
             console.log("Button clicked!");
             this.form = {checked: this.checked, fullname: this.fullname, email: this.email, payment: this.payment, gender: this.gender, coordinates: this.coordinates};
-            //this.addOrder;
+            this.addOrder();
         },
         getNext: function () {
             var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
@@ -38,13 +38,13 @@ var vueObject = new Vue({
             }, 0);
             return lastOrder + 1;
           },
-          addOrder: function (event) {
+          addOrder: function() {
             var offset = {x: event.currentTarget.getBoundingClientRect().left,
                           y: event.currentTarget.getBoundingClientRect().top};
             socket.emit("addOrder", { orderId: this.getNext(),
-                                      details: { x: event.clientX - 10 - offset.x,
-                                                 y: event.clientY - 10 - offset.y },
-                                      orderItems: ["Beans", "Curry"]
+                                      details: { x: this.coordinates.x,
+                                                 y: this.coordinates.y },
+                                      orderItems: this.checked
                                     });
           },
           displayOrder: function (event) {
